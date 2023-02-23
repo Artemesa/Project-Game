4e123cf2d1bca37b079a106f5673dcdcf9245810
Daniel <danielsotirov3@gmail.com>
2023-02-23 18:40:50 +0200

    done i think (i hope)

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ let ny = 11;
 let sqside = 24;
 let herox = 2;
 let heroy = 1;
+let villainx = 5;
+let villainy = 6;
+let hasvillainspawned = false;
 let r = 16;
 let a = Math.PI/3; //angle
+let h = r * Math.sqrt(3) / 2;
 let heroimg = new Image();
-heroimg.src = "hero.png";
-canvas.width = nx * r * 2;
-canvas.height =  ny * r * 2;
+let villainimg = new Image();
+heroimg.src = "devil.png";
+villainimg.src = "durvo.png";
+canvas.width = nx * h * 2;
+canvas.height =  ny * (2*r - Math.sqrt(r ** 2 - h ** 2)) + Math.sqrt(r ** 2 - h ** 2);
 let map = [];
 
 function Hexagon(x, y, isFill) {
@@ -21,24 +27,30 @@ function Hexagon(x, y, isFill) {
 mapGen(canvas.width, canvas.height);
 function drawMap() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(heroimg, herox, heroy, sqside, sqside);
+    context.drawImage(villainimg, villainx - 5, villainx - 55, 30, 75);
+    context.drawImage(heroimg, herox - 5, heroy - 50, 30, 75);
     drawGrid(canvas.width, canvas.height);
 }
 drawMap();
 function mapGen(width, height){
-     for (let x = r, j = 0; x + r * Math.sin(a) < height; x += 2 ** ((j + 1) % 2) * r * Math.sin(a), j = 0) {
-        for (let y = r; y + r * (1 + Math.cos(a)) < width; y += r * (1 + Math.cos(a)), x += (-1) ** j++ * r * Math.sin(a)) {
+     for (let y = r, i = 0, j = 0; y < height; y += 2 * r - Math.sqrt(r ** 2 - h ** 2), j++) {
+        for (let x = (j % 2==0 ? 2 * h : h);  x < width; x += h * 2, i++){
             let isFill = false;
             if(Math.random() * 176 <= 3){
                 isFill = true;
+            }
+            else if(Math.random() * 100 <= 1 && !hasvillainspawned){
+                hasvillainspawned = true;
+                villainx = x - 2 * r / 3;
+                villainy = y - 2 * r / 3;
             }
             map.push(new Hexagon(x, y, isFill));
         }
     }
 }
 function drawGrid(width, height) {
-    for (let x = r, j = 0, i = 0; x + r * Math.sin(a) < height; x += 2 ** ((j + 1) % 2) * r * Math.sin(a), j = 0) {
-        for (let y = r; y + r * (1 + Math.cos(a)) < width; y += r * (1 + Math.cos(a)), x += (-1) ** j++ * r * Math.sin(a), i++) {
+    for (let y = r, i = 0, j = 0; y < height; y += 2 * r - Math.sqrt(r ** 2 - h ** 2), j++) {
+        for (let x = (j % 2==0 ? 2 * h : h);  x < width; x += h * 2, i++){
             drawHexagon(x, y, map[i].isFill);
         }
     }
@@ -69,3 +81,12 @@ canvas.onclick = function(e) {
     }
     drawMap();
 }
+
+
+
+
+
+
+
+
+//भारतीयों और उनकी माताओं को बहुत-बहुत धन्यवाद <3 от Дани
