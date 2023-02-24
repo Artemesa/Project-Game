f3ef3ce0869e6fceb7d8124f2264c2121f6a3642
Daniel <daniel.j.sotirov@gmail.com>
2023-02-24 12:30:40 +0200

    final

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ let canvas = document.getElementById("gameCanvas");
 let context = canvas.getContext("2d");
 let nx = 16;
 let ny = 11;
-let sqside = 24;
+//let sqside = 24;
 let herox = 2;
 let heroy = 1;
 let villainx = 5;
 let villainy = 6;
 let hasvillainspawned = false;
 let r = 16;
-let a = Math.PI/3; //angle
+let a = Math.PI / 3; //angle
 let h = r * Math.sqrt(3) / 2;
 let heroimg = new Image();
 let villainimg = new Image();
 heroimg.src = "devil.png";
 villainimg.src = "durvo.png";
 canvas.width = nx * h * 2;
-canvas.height =  ny * (2*r - Math.sqrt(r ** 2 - h ** 2)) + Math.sqrt(r ** 2 - h ** 2);
+canvas.height = ny * (2 * r - Math.sqrt(r ** 2 - h ** 2)) + Math.sqrt(r ** 2 - h ** 2);
 let map = [];
 
 function Hexagon(x, y, isFill) {
@@ -25,6 +25,7 @@ function Hexagon(x, y, isFill) {
     this.isFill = isFill;
 }
 mapGen(canvas.width, canvas.height);
+
 function drawMap() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.drawImage(villainimg, villainx - 5, villainx - 55, 30, 75);
@@ -32,14 +33,14 @@ function drawMap() {
     drawGrid(canvas.width, canvas.height);
 }
 drawMap();
-function mapGen(width, height){
-     for (let y = r, i = 0, j = 0; y < height; y += 2 * r - Math.sqrt(r ** 2 - h ** 2), j++) {
-        for (let x = (j % 2==0 ? 2 * h : h);  x < width; x += h * 2, i++){
+
+function mapGen(width, height) {
+    for (let y = r, i = 0, j = 0; y < height; y += 2 * r - Math.sqrt(r ** 2 - h ** 2), j++) {
+        for (let x = (j % 2 == 0 ? 2 * h : h); x < width; x += h * 2, i++) {
             let isFill = false;
-            if(Math.random() * 176 <= 3){
+            if (Math.random() * 176 <= 3) {
                 isFill = true;
-            }
-            else if(Math.random() * 100 <= 1 && !hasvillainspawned){
+            } else if (Math.random() * 100 <= 1 && !hasvillainspawned) {
                 hasvillainspawned = true;
                 villainx = x - 2 * r / 3;
                 villainy = y - 2 * r / 3;
@@ -48,22 +49,24 @@ function mapGen(width, height){
         }
     }
 }
+
 function drawGrid(width, height) {
     for (let y = r, i = 0, j = 0; y < height; y += 2 * r - Math.sqrt(r ** 2 - h ** 2), j++) {
-        for (let x = (j % 2==0 ? 2 * h : h);  x < width; x += h * 2, i++){
+        for (let x = (j % 2 == 0 ? 2 * h : h); x < width; x += h * 2, i++) {
             drawHexagon(x, y, map[i].isFill);
         }
     }
 }
+
 function drawHexagon(x, y, isFill) {
     context.beginPath();
     for (let i = 0; i < 6; i++) {
         context.lineTo(x + r * Math.sin(a * i), y + r * Math.cos(a * i));
     }
     context.closePath();
-    if(isFill){
+    if (isFill) {
         context.fill();
-    } else{
+    } else {
         context.stroke();
     }
 }
@@ -71,9 +74,9 @@ canvas.onclick = function(e) {
     let x = e.x - canvas.offsetLeft;
     let y = e.y - canvas.offsetTop;
     let h = Math.sqrt(3) * r / 2;
-    for(let hex of map){
+    for (let hex of map) {
         let distance = Math.sqrt((hex.x - x) ** 2 + (hex.y - y) ** 2);
-        if(distance < h && !hex.isFill){
+        if (distance < h && !hex.isFill) {
             herox = hex.x - 2 * r / 3;
             heroy = hex.y - 2 * r / 3;
             break;
@@ -81,12 +84,3 @@ canvas.onclick = function(e) {
     }
     drawMap();
 }
-
-
-
-
-
-
-
-
-//भारतीयों और उनकी माताओं को बहुत-बहुत धन्यवाद <3 от Дани
